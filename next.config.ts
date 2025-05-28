463016200c2665c2d35e0cf4e47dcd61ffd9fc75
Daniel Bonnin <danielsbonnin@gmail.com>
2025-05-28 16:02:45 -0500

    Fix basePath to match repository name 'dsbdotcom'

--- next.config.ts
+++ next.config.ts
@@ -11,8 +11,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: isProd && isGitHubPages ? '/danielsbonnin.com' : '',
-  assetPrefix: isProd && isGitHubPages ? '/danielsbonnin.com/' : '',
+  basePath: isProd && isGitHubPages ? '/dsbdotcom' : '',
+  assetPrefix: isProd && isGitHubPages ? '/dsbdotcom/' : '',
 };
 
 export default nextConfig;
